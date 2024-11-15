3c6fd5f57e6eaa61262060192207f2de4ba6e8bf
Francisco Rondinelly <RondiTest@Dev.com>
2024-11-15 14:05:22 -0300

    Introdução de orientação a objectos

---
+++ Aluas/Introdução_OOP/Aula01_function/class_in_functions.js
@@ -0,0 +1,9 @@
+//basicamente na orientação a objetos,crimos classes e objetos 
+//nesse caso teremos uma function como comportamento de class
+function Pessoa(name) {
+  this.name = name
+  //ele returna automaticamente um object
+}
+//instanciando objct 
+let p1 = new Pessoa("seu ZÉ")
+console.log(p1)
