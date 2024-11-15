d384e595f90f2f77d30d993fd52216276e90017d
Francisco Rondinelly <RondiTest@Dev.com>
2024-11-15 07:42:02 -0300

    aprendendo sobre vatiaves primitivas!

---
+++ Aluas/Alua01_variaves/variaves.js
@@ -0,0 +1,19 @@
+//existem muitos tipos de variavew mas no javascript são:
+//number,string,undefinde,object,boolean,arrays e etc.
+//boolean
+var bol = true
+if (bol){
+  console.log("bol é Verdadeiro!")
+}
+else {
+  console.error("bol é Falso")
+}
+//strings
+let hello = "Hello Word"
+console.log(`${hello}`)
+//number
+function somar(n1,n2){
+  return n1+n2
+}
+console.log(`${somar(12,34)}`)
+
