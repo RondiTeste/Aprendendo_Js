f7639a8ecdabfad8237668ce3d0bb850fd751009
Francisco Rondinelly <RondiTest@Dev.com>
2024-11-15 14:59:32 -0300

    metodo estatico

---
+++ Aluas/Introdução_OOP/Aula02Sytax_class/static_class.js
@@ -0,0 +1,9 @@
+class Maths {
+  static somar(n1 , n2){
+    return n1 + n2
+  }
+}
+mat = Maths.somar(2,5)
+console.log(mat)
+//nesse caso não precisa instaciar
+//static define se o metodo é estatico
